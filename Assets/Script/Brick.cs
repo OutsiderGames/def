3670649f53aa71f8fddca1a4c500136d112375ea
Argent <ddkkinf@naver.com>
2016-02-13 12:09:15 +0900

    brick 작업 중 / test scene 추가

--- Assets/Script/Brick.cs
+++ Assets/Script/Brick.cs
@@ -5,6 +5,10 @@ public class Brick : MonoBehaviour {
 	private SpriteRenderer spriteRenderer;
 	private float b = 0.9f;
 	private float increment = 0.005f;
+	private float horizonSize = 0.96f;
+	private float verticalSize = 0.6f;
+	private float[] horizonPositions = {-2.4f, -1.44f, -0.48f, 0.48f, 1.44f, 2.4f};
+	private float[] verticalPositions = {-2.4f, -1.8f, -1.2f, -0.6f, 0.0f, 0.6f, 1.2f, 1.8f, 2.4f};
 
 	// Use this for initialization
 	void Start () {
